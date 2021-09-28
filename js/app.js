b637bef93dff0a92bb022e43aa03a247ae5eb3a2
Pablo <paablo.92@hotmail.es>
2021-09-28 16:48:23 -0300

    ejercicio 1 terminado

--- js/app.js
+++ js/app.js
@@ -1,11 +1,15 @@
+
+let numero;
+
+
 function numeroAzar() {
   numero = Math.floor(Math.random() * 11);
   console.log(numero)
-  return numero
+
 }
 
 function Enviar() {
-  let numeroEnviado = parseInt(document.getElementById(`numeroIngresado`));
+  let numeroEnviado = parseInt(document.getElementById(`numeroIngresado`).value);
   console.log(numeroEnviado);
 
   if (numeroEnviado == numero) {
